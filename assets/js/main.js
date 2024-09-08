0a17c564ad044b03c6a5e407fc75cf666ef15b9f
Md. Omar Faruk <87226097+md-omarfaruk@users.noreply.github.com>
2024-09-08 17:24:23 +0600

    tried to fix header menu now maybe loking prety good

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,17 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 103 || document.documentElement.scrollTop > 103) {
-    document.getElementById("fixed-nav-logo").style.display = "inline";
-    document.getElementById("header-navbar-one").style.width = "100%";
-    document.getElementById("header-navbar-one").style.position = "fixed";
-    document.getElementById("header-navbar-one").style.top = "0px";
-    document.getElementById("header-navbar-one").style.left = "0px";
+  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+    document.getElementById("header-navbar-two").style.display = "inline-block";
+    document.getElementById("header-navbar-one").style.display = "none";
+    document.getElementById("header-navbar-two").style.width = "100%";
+    document.getElementById("header-navbar-two").style.position = "fixed";
+    document.getElementById("header-navbar-two").style.top = "0px";
+    document.getElementById("header-navbar-two").style.left = "0px";
 
   } else {
-    document.getElementById("fixed-nav-logo").style.display = "none";
+    document.getElementById("header-navbar-two").style.display = "none";
+    document.getElementById("header-navbar-one").style.display = "inline-block";
     document.getElementById("header-navbar-one").style.width = "100%";
     document.getElementById("header-navbar-one").style.position = "fixed";
     document.getElementById("header-navbar-one").style.top = "103px";
