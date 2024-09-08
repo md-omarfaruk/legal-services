40e92216d78c66f76544ed521a9e9b86994733a8
Md. Omar Faruk <87226097+md-omarfaruk@users.noreply.github.com>
2024-09-08 20:09:00 +0600

    navbar all issues and responsive issues are fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,22 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
-    document.getElementById("header-navbar-two").style.display = "inline-block";
-    document.getElementById("header-navbar-one").style.display = "none";
-    document.getElementById("header-navbar-two").style.position = "fixed";
-    document.getElementById("header-navbar-two").style.top = "0px";
-    document.getElementById("header-navbar-two").style.left = "0px";
+    const mediaQuery = window.matchMedia(['(max-width: 767.98px)', '(max-width: 991.98px)'])
+    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+        document.getElementById("header-navbar-two").style.display = "inline-block";
+        document.getElementById("header-navbar-one").style.display = "none";
+        document.getElementById("header-navbar-two").style.position = "fixed";
+        document.getElementById("header-navbar-two").style.top = "0px";
+        document.getElementById("header-navbar-two").style.left = "0px";
+        if (mediaQuery.matches) {
+            document.getElementById("header-navbar-two").style.display = "none";
+        }
 
-  } else {
-    document.getElementById("header-navbar-two").style.display = "none";
-    document.getElementById("header-navbar-one").style.display = "inline-block";
-    document.getElementById("header-navbar-one").style.width = "100%";
-  }
+    } else {
+        document.getElementById("header-navbar-two").style.display = "none";
+        document.getElementById("header-navbar-one").style.display = "inline-block";
+        document.getElementById("header-navbar-one").style.width = "100%";
+    }
 }
 
 // -----------------------------jQuery-Animations--------------------------------
