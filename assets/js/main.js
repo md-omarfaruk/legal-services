2c0b09bd330ccf873c6df89a3fc3e2553bbf65c1
Md. Omar Faruk <87226097+md-omarfaruk@users.noreply.github.com>
2024-09-08 18:30:07 +0600

    name and image changed from that fb profile and header menu edited

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,17 +4,20 @@ function scrollFunction() {
   if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
     document.getElementById("header-navbar-two").style.display = "inline-block";
     document.getElementById("header-navbar-one").style.display = "none";
-    document.getElementById("header-navbar-two").style.width = "100%";
     document.getElementById("header-navbar-two").style.position = "fixed";
     document.getElementById("header-navbar-two").style.top = "0px";
     document.getElementById("header-navbar-two").style.left = "0px";
+    document.getElementById("header-navbar-two").style.transition-duration = 1000;
 
   } else {
     document.getElementById("header-navbar-two").style.display = "none";
     document.getElementById("header-navbar-one").style.display = "inline-block";
     document.getElementById("header-navbar-one").style.width = "100%";
-    document.getElementById("header-navbar-one").style.position = "fixed";
-    document.getElementById("header-navbar-one").style.top = "103px";
-    document.getElementById("header-navbar-one").style.left = "0px";
   }
 }
+
+// -----------------------------jQuery-Animations--------------------------------
+
+$(document).ready(function(){
+      $("#slidUpSlowAnimate").slideUp("slow");
+  });
