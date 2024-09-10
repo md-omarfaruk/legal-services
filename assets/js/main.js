ca6877ea6a6ebc24b7e39b09f9e8412cf1443426
Md. Omar Faruk <87226097+md-omarfaruk@users.noreply.github.com>
2024-09-10 20:22:24 +0600

    gallery page is completed and git added

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,29 +20,75 @@ function scrollFunction() {
     }
 }
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const element =document.getElementById("fadeInUp");
-//    element.classList.add("fadeInUp");
-//   });
+// --------------------Gallery-Lightbox-Script-Start--------------------------------
 
-const lightbox = document.createElement('div')
-lightbox.id = 'lightbox'
-document.body.appendChild(lightbox)
+// query selectors
+const lightboxEnabled = document.querySelectorAll('.lightbox-enabled');
+const lightboxArray = Array.from(lightboxEnabled);
+const lastImage = lightboxArray.length-1;
+const lightboxContainer = document.querySelector('.lightbox-container');
+const lightboxImage = document.querySelector('.lightbox-image');
+const lightboxBtns = document.querySelectorAll('.lightbox-btn');
+const lightboxBtnRight = document.querySelector('#right');
+const lightboxBtnLeft = document.querySelector('#left');
+const close = document.querySelector('#close');
+let activeImage;
+// Functions
+const showLightBox = () => {lightboxContainer.classList.add('active')}
 
-const images = document.querySelectorAll('img')
-images.forEach(image => {
-  image.addEventListener('click', e => {
-    lightbox.classList.add('active')
-    const img = document.createElement('img')
-    img.src = image.src
-    while (lightbox.firstChild) {
-      lightbox.removeChild(lightbox.firstChild)
-    }
-    lightbox.appendChild(img)
-  })
+const hideLightBox = () => {lightboxContainer.classList.remove('active')}
+
+const setActiveImage = (image) => {
+lightboxImage.src = image.dataset.imgsrc;
+activeImage= lightboxArray.indexOf(image);
+}
+
+const transitionSlidesLeft = () => {
+  lightboxBtnLeft.focus();
+  $('.lightbox-image').addClass('slideright'); 
+   setTimeout(function() {
+  activeImage === 0 ? setActiveImage(lightboxArray[lastImage]) : setActiveImage(lightboxArray[activeImage-1]);
+}, 250); 
+
+
+  setTimeout(function() {
+    $('.lightbox-image').removeClass('slideright');
+}, 500);   
+}
+
+const transitionSlidesRight = () => {
+ lightboxBtnRight.focus();
+$('.lightbox-image').addClass('slideleft');  
+  setTimeout(function() {
+   activeImage === lastImage ? setActiveImage(lightboxArray[0]) : setActiveImage(lightboxArray[activeImage+1]);
+}, 250); 
+  setTimeout(function() {
+    $('.lightbox-image').removeClass('slideleft');
+}, 500);  
+}
+
+const transitionSlideHandler = (moveItem) => {
+  moveItem.includes('left') ? transitionSlidesLeft() : transitionSlidesRight();
+}
+
+// Event Listeners
+lightboxEnabled.forEach(image => {
+   image.addEventListener('click', (e) => {
+    showLightBox();
+    setActiveImage(image);
+    })                     
+    })
+lightboxContainer.addEventListener('click', () => {hideLightBox()})
+close.addEventListener('click', () => {hideLightBox()})
+lightboxBtns.forEach(btn => {
+btn.addEventListener('click', (e) => {
+e.stopPropagation();
+  transitionSlideHandler(e.currentTarget.id);
+})
 })
 
-lightbox.addEventListener('click', e => {
-  if (e.target !== e.currentTarget) return
-  lightbox.classList.remove('active')
+lightboxImage.addEventListener('click', (e) => {
+e.stopPropagation();
+
 })
+// --------------------Gallery-Lightbox-Script-End--------------------------------
